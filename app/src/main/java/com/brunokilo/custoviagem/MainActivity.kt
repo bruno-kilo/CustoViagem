5ada17446a57e15b3b81ea5f86117b456162fb87
“[Bruno Kilo <brunesilva@gmail.com>
2023-10-18 15:39:54 -0300

    First APP Kotlin

--- app/src/main/java/com/brunokilo/custoviagem/MainActivity.kt
+++ app/src/main/java/com/brunokilo/custoviagem/MainActivity.kt
@@ -1,56 +1,60 @@
 package com.brunokilo.custoviagem
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.brunokilo.custoviagem.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.root
-        setContentView(binding.root)
 
-        binding.buttonCalculate.setOnClickListener(this)
-
-    }
-
-    override fun onClick(view: View) {
-        if (view.id == R.id.button_calculate) {
-            calculate()
-        } else {
-            isValid()
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+            buttonCalculate.setOnClickListener {
+                calculate()
+            }
         }
+
     }
 
+    // Apply não aplicável em Boolean, somente usando .run
+    // Aceitei a sugestão do Kotlin e ele gerou o == true
     private fun isValid(): Boolean {
-        return (binding.editDistance.text.toString() != ""
-                && binding.editPrice.text.toString() != ""
-                && binding.editAutonomy.text.toString() != ""
-                && binding.editAutonomy.text.toString().toFloat() != 0f)
+        return (binding?.editDistance?.text?.isNotEmpty() == true
+                && binding?.editPrice?.text?.isNotEmpty() == true
+                && binding?.editAutonomy?.text?.isNotEmpty() == true)
     }
+
     private fun calculate() {
-
         if (isValid()) {
-
-            val distance = binding.editDistance.text.toString().toFloat()
-            val price = binding.editPrice.text.toString().toFloat()
-            val autonomy = binding.editAutonomy.text.toString().toFloat()
-            val totalValue = (distance * price) / autonomy
-            binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+            binding?.apply {
+                val distance = editDistance.toFormatFloat()
+                val price = editPrice.toFormatFloat()
+                val autonomy = editAutonomy.toFormatFloat()
+                val totalValue = (distance * price) / autonomy
+                textTotalValue.text = totalValue.formatFloat()
+            }
 
             // Toast notification
             // Toast.makeText(this, totalValueStr, Toast.LENGTH_SHORT).show()
         } else {
             Toast.makeText(this, R.string.validation_fill_all_fields, Toast.LENGTH_SHORT).show()
         }
-        }
+    }
+
+    private fun Float.formatFloat(): String {
+        return "R$ ${"%.2f".format(this)}"
+    }
+
+    private fun EditText.toFormatFloat(): Float {
+        return this.text.toString().toFloat()
+    }
 
 }
 
